bc3c3ca363e71f9cd475c6e8d98430b2331bde3d
Bes <sejrani.besjan@gmail.com>
2019-05-25 18:41:15 +0200

    fixed cart total issue 3

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -20,7 +20,7 @@ const CartTotals = ({ cartTotal, cartSubTotal, cartTax }) => {
             </button>
             <h3>subTotal: {Number(cartSubTotal)} CHF</h3>
             <h3>tax : {Number(cartTax)} CHF</h3>
-            <h3>total: {cartSubTotal + cartTax} CHF</h3>
+            <h3>total: {parseFloat(cartSubTotal + cartTax).toFixed(2)} CHF</h3>
           </div>
         </div>
       </div>
