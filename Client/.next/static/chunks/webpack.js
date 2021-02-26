f2ab2d1ab66a0009ba7b9b25ed376d2b35756d47
besSejrani <sejrani.besjan@gmail.com>
2021-02-26 04:05:35 +0100

    deployed server on Heroku

--- Client/.next/static/chunks/webpack.js
+++ Client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5df317321b9a595efc12";
+/******/ 	var hotCurrentHash = "e1ef34c245824416917f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
